ced388f0d86594f8b1a19755ab173dc2ce40a4ea
RobMengelers <robmengelers@gmail.com>
2023-03-09 09:48:05 +0100

    PHPMailer

---
+++ credentials.php
@@ -0,0 +1,10 @@
+<?php
+    if (!defined('EMAIL')) {
+        define('EMAIL', 'info@splitly.nl');
+    }
+
+    if (!defined('PASS')) {
+        define('PASS', 'Mengelers1!');
+    }
+
+?>
---
+++ sendmail.php
@@ -0,0 +1,45 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+// require 'PHPMailer-master/src/Exception.php';
+// require 'PHPMailer-master/src/PHPMailer.php';
+// require 'PHPMailer-master/src/SMTP.php';
+
+require 'credentials.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.zoho.eu';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = EMAIL;                     //SMTP username
+    $mail->Password   = PASS;                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom(EMAIL, 'RobM');
+    $mail->addAddress('robmengelers@gmail.com', 'Joe User');     //Add a recipient
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
